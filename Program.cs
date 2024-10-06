97a42f9472630d9dd8f433a908e65fe594799d50
rafaelpac <rafaelpac@proton.me>
2024-10-06 19:39:03 -0300

    desafio smartphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456789", modelo: "P3650", imei: "111111111xxx", memoria: 4);
+nokia.Ligar();
+nokia.InstalarAplicativo("Cobrinha - Snake");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "987654321", modelo: "2", imei: "3333333yyyyy", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Maps");
